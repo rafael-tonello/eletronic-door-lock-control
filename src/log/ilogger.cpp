399327d6d62eaed8ff139ddeb9983c537a915237
Rafael Tonello <tonello.rafinha@gmail.com>
2026-04-08 14:42:11 +0000

    feat: add support for color on log system

--- src/log/ilogger.cpp
+++ src/log/ilogger.cpp
@@ -1,5 +1,53 @@
 #include  "ilogger.h" 
 
+String FF::Reset = "\033[0m";
+
+String FF::cl_Red = "\033[31m";
+String FF::cl_Green = "\033[32m";
+String FF::cl_Yellow = "\033[33m";
+String FF::cl_Blue = "\033[34m";
+String FF::cl_Magenta = "\033[35m";
+String FF::cl_Cyan = "\033[36m";
+String FF::cl_White = "\033[37m";
+String FF::cl_BrightRed = "\033[91m";
+String FF::cl_BrightGreen = "\033[92m";
+String FF::cl_BrightYellow = "\033[93m";
+String FF::cl_BrightBlue = "\033[94m";
+String FF::cl_BrightMagenta = "\033[95m";
+String FF::cl_BrightCyan = "\033[96m";
+String FF::cl_BrightWhite = "\033[97m";
+
+String FF::bg_Red = "\033[41m";
+String FF::bg_Green = "\033[42m";
+String FF::bg_Yellow = "\033[43m";
+String FF::bg_Blue = "\033[44m";
+String FF::bg_Magenta = "\033[45m";
+String FF::bg_Cyan = "\033[46m";
+String FF::bg_White = "\033[47m";
+String FF::bg_BrightRed = "\033[101m";
+String FF::bg_BrightGreen = "\033[102m";
+String FF::bg_BrightYellow = "\033[103m";
+String FF::bg_BrightBlue = "\033[104m";
+String FF::bg_BrightMagenta = "\033[105m";
+String FF::bg_BrightCyan = "\033[106m";
+String FF::bg_BrightWhite = "\033[107m";
+
+String FF::st_Bold = "\033[1m";
+String FF::st_Dim = "\033[2m";
+String FF::st_Underlined = "\033[4m";
+String FF::st_Blink = "\033[5m";
+String FF::st_Inverted = "\033[7m";
+String FF::st_Hidden = "\033[8m";
+String FF::st_Strikethrough = "\033[9m";
+String FF::st_Bright = "\033[1m";
+String FF::st_DimBright = "\033[2m";
+String FF::st_UnderlinedBright = "\033[4m";
+String FF::st_BlinkBright = "\033[5m";
+String FF::st_InvertedBright = "\033[7m";
+String FF::st_HiddenBright = "\033[8m";
+
+shared_ptr<ILogger> DefaultLogger::__defaultInstance = nullptr;
+
 void ILogger::log(LogLevel level, String name, String msg, bool breakLine)
 {
     this->logIt(level, name, msg, breakLine);
@@ -56,17 +104,18 @@ String ILogger::getDateTimeInfo()
     //return String("");
 }
 
-String ILogger::identMsg(String msg, int identationSize)
+String ILogger::identMsg(String msg, unsigned int identationSize)
 {
-    String identationChars ="                                                                                                    ";
+    //pre allocate identation chars
+    String identationChars = "                                                   ";
     //identationPrefix.reserve(identationSize);
 
 
-    for (int c = identationChars.length(); c < identationSize; c++)
-        identationChars+=' ';
-
+    while (identationChars.length() < identationSize)
+        identationChars += "    ";
 
     identationChars = identationChars.substring(0, identationSize);
+
     msg.replace("\n", "\n"+identationChars);
 
     return msg;
@@ -75,7 +124,7 @@ String ILogger::identMsg(String msg, int identationSize)
 String ILogger::MountLineHeader(LogLevel level, String name, bool alloLinuxColors)
 {
     String header = "";
-    if (LOGGER_ENABLE_LINUX_COLORS)
+    if (alloLinuxColors)
     {
         //close previous color (if any)
         header += "\033[0m";
@@ -104,7 +153,7 @@ String ILogger::MountLineHeader(LogLevel level, String name, bool alloLinuxColor
 
     String dateTime = getDateTimeInfo();
     if (dateTime != "")
-        header = "["+dateTime+"] "+ header;
+        header += "["+dateTime+"] "+ header;
 
     header += "["+LogLevelToString(level)+"] ";
     header += "["+name+"] ";
@@ -135,6 +184,9 @@ void DefaultLogger::logIt(LogLevel level, String name, String msg, bool breakLin
 
     msg = header + msg;
 
+    if (allowLinuxColors)
+        msg += FF::Reset;
+
     Serial.print(msg.c_str());
     //msg.clear();
 
@@ -145,4 +197,23 @@ void DefaultLogger::logIt(LogLevel level, String name, String msg, bool breakLin
         newLineOwnerName = name;
 
     isANewLine = breakLine;
+
+    
 }
+
+DefaultLogger::DefaultLogger(
+    bool allowLinuxColors,
+    bool setAsDefaultIntance
+): allowLinuxColors(allowLinuxColors) {
+    if (setAsDefaultIntance)
+        DefaultLogger::__defaultInstance = shared_ptr<ILogger>(this);
+}
+
+shared_ptr<ILogger> DefaultLogger::DefaultInstance(bool allowLinuxColors)
+{
+    if (__defaultInstance == nullptr)
+        __defaultInstance = make_shared<DefaultLogger>(allowLinuxColors);
+
+    return __defaultInstance;
+}
+
--- src/log/ilogger.h
+++ src/log/ilogger.h
@@ -10,9 +10,10 @@ using namespace std;
  
 enum LogLevel {DEBUG, INFO, WARNING, ERROR, CRITICAL};
 
-class NamedLog;
 
-#define LOGGER_ENABLE_LINUX_COLORS true
+
+
+class NamedLog;
 
 struct ILoggerObservingItem{
     String name;
@@ -35,7 +36,57 @@ protected:
     
 
     
-public: 
+public:
+    class LinuxFontFeatures {
+    public:
+        static String Reset;
+
+        static String cl_Red;
+        static String cl_Green;
+        static String cl_Yellow;
+        static String cl_Blue;
+        static String cl_Magenta;
+        static String cl_Cyan;
+        static String cl_White;
+        static String cl_BrightRed;
+        static String cl_BrightGreen;
+        static String cl_BrightYellow;
+        static String cl_BrightBlue;
+        static String cl_BrightMagenta;
+        static String cl_BrightCyan;
+        static String cl_BrightWhite;
+
+        static String bg_Red;
+        static String bg_Green;
+        static String bg_Yellow;
+        static String bg_Blue;
+        static String bg_Magenta;
+        static String bg_Cyan;
+        static String bg_White;
+        static String bg_BrightRed;
+        static String bg_BrightGreen;
+        static String bg_BrightYellow;
+        static String bg_BrightBlue;
+        static String bg_BrightMagenta;
+        static String bg_BrightCyan;
+        static String bg_BrightWhite;
+
+        static String st_Bold;
+        static String st_Dim;
+        static String st_Underlined;
+        static String st_Blink;
+        static String st_Inverted;
+        static String st_Hidden;
+        static String st_Strikethrough;
+        static String st_Bright;
+        static String st_DimBright;
+        static String st_UnderlinedBright;
+        static String st_BlinkBright;
+        static String st_InvertedBright;
+        static String st_HiddenBright;
+        static String st_StrikethroughBright;
+    };
+
     EventStream<ILoggerObservingItem> OnLog;
     virtual ~ILogger() = default;
 
@@ -48,11 +99,13 @@ public:
     virtual String MountLineHeader(LogLevel level, String name, bool alloLinuxColors);
     
     //a helper that ident the message with the specified number of spaces (usefull to align multiline messages)
-    String identMsg(String msg, int identationSize);
+    String identMsg(String msg, unsigned int identationSize);
 
     NamedLog getNLog(String name);
     shared_ptr<NamedLog> getNLogP(String name);
-}; 
+};
+
+#define FF ILogger::LinuxFontFeatures
 
 class NamedLog{
 private:
@@ -120,9 +173,14 @@ private:
     String lastHeader = "";
     bool allowLinuxColors = true;
 
+    static shared_ptr<ILogger> __defaultInstance;
 public:
-    DefaultLogger(bool allowLinuxColors = true): allowLinuxColors(allowLinuxColors) {}
+
+    DefaultLogger(bool allowLinuxColors = true, bool setAsDefaultIntance = true);
+
     void logIt(LogLevel level, String name, String msg, bool breakLine = true) override;
+
+    static shared_ptr<ILogger> DefaultInstance(bool allowLinuxColors = true);
 
 };
  
