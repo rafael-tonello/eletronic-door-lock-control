83078597203ad7e7ee6e57a925d61c083dfa93fb
Rafael Tonello <tonello.rafinha@gmail.com>
2026-04-08 14:44:33 +0000

    feat: add lock/unlock logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-#include <errors/errors.h>
 
+#include <vector>
+
+#include <errors/errors.h>
 #include <scheduler.h>
+
+#include <iiohal.h>
+#include <iohal/impl/Esp8266_74hc595_hc4067/Esp8266_74hc595_hc4067.h>
 
 #include <ikeyboard.h>
 #include <keyboard/impl/GenericIIOHalKeyboard.h>
-
-#include <iiohal.h>
-#include <iohal/impl/esp8266IOHal.h>
 
 #include <ilogger.h>
 
@@ -16,148 +18,173 @@
 #include <storage/impl/preferenceslibrary/preferenceslibrarystorage.h>
 
 #include <inetwork.h>
-
-#include <network/inetwork.h>
 #include <network/impl/wfservice.h>
 
 #include <telnet/telnetserver.h>
 
+#include <vssClient/vstpclient.h>
+
 #include "main.telnetui.h"
 
-Scheduler scheduler;
+#include "main.lockcontrol.h"
 
-IIOHal *hal;
-ILogger *logService;
-IStorage *storage;
-INetwork *conService;
-IKeyboard* keyboard; //= new KeyBoardImplementation();
-TelnetServer *telnetInterface; //= new TelnetServer(*logService, *storage, *conService, scheduler);
-MainTelnetUI* mainTelnetUI;
-bool otaStarted = false;
-
-bool tmp = false;
-
-void setup() {
-    String initailTelnetBanner = "\n";
-    initailTelnetBanner += "===========================\n"; 
-    initailTelnetBanner += "     Door lock control     \n"; 
-    initailTelnetBanner += "===========================\n";
+using namespace std;
 
 
 
+namespace {
+    String VERSION = "v0.0.1";
+    const String DEFAULT_PREFIX = "n1.sys.doorlock.";
+    const String WIFI_HOSTNAME = "ESP8266-GENIO";
+
+    const auto ENDLINE_LOCK_INPUT_PIN = IOHAL_A14;
+    const auto ENDLINE_UNLOCK_INPUT_PIN = IOHAL_A15;
+    const auto MOTOR_ENABLE_OUTPUT_PIN = IOHAL_D0;
+    const auto MOTOR_UNLOCK_OUTPUT_PIN = IOHAL_D1;
+    const auto MOTOR_LOCK_OUTPUT_PIN = IOHAL_D2;
+
+    const auto KEY_LOCK = IOHAL_A13;
+    const auto KEY_UNLOCK = IOHAL_A12;
+
+
+    Scheduler scheduler;
+
+    IIOHal *hal = nullptr;
+    ILogger *logService = nullptr;
+    IStorage *storage = nullptr;
+    INetwork *conService = nullptr;
+    IKeyboard *keyboard = nullptr;
+    TelnetServer *telnetInterface = nullptr;
+    MainTelnetUI *mainTelnetUI = nullptr;
+    VSTP::VstpClient *vssCli = nullptr;
+
+    NamedLog nLog;
+    MainLockControl *lockControl = nullptr;
+}
+
+bool tmp = true;
+uint timeout = 26500;
+
+void setup()
+{
+    /* #region esp setup */
     Serial.begin(115200);
     delay(1000);
 
     ArduinoOTA.setHostname("espota");
     ArduinoOTA.begin();
+    /* #endregion */
 
-    hal = new ESP8266IOHal();
-    keyboard = new GenericIIOHalKeyboard(*hal, scheduler, {IOHAL_D1});
-    logService = new DefaultLogger();
-    storage = new PreferencesLibraryStorage(scheduler);
-    conService = new WFService(scheduler, *logService, *storage);
-    Serial.println("conservice address: " + String((uintptr_t)conService));
+    String initialTelnetBanner = "\n";
+    initialTelnetBanner += "===========================\n";
+    initialTelnetBanner += "     Door lock control     \n";
+    initialTelnetBanner += "===========================\n";
+    String telnetHelpTitleLine = "Door lock control system, version " + VERSION;
 
-    telnetInterface = new TelnetServer(initailTelnetBanner, "humandis", scheduler, *storage, *conService, *logService);
+    /* #region modules initialization { */
+        hal = new ESP8266_74hc595_hc4067();
 
-
-
-    mainTelnetUI = new MainTelnetUI(*hal, *logService, *storage, *conService, *keyboard, *telnetInterface);
-
-    ((WFService*)conService)->connectToNetwork("Tonello", "tetelateteka")->then([&](Error err){
+        keyboard = new GenericIIOHalKeyboard(*hal, scheduler, {KEY_LOCK, KEY_UNLOCK});
         
-    });
+        logService = new DefaultLogger(true);
+        nLog = logService->getNLog("Main");
 
+        storage = new PreferencesLibraryStorage(scheduler);
+        
+        conService = new WFService(scheduler, *logService, *storage, WIFI_HOSTNAME);
+        
+        telnetInterface = new TelnetServer(initialTelnetBanner, "humandis", scheduler, *storage, *conService, *logService);
 
-    scheduler.periodicTask(100 MilliSeconds, [&](){
-        auto v = hal->Read(IOHAL_D3).Digital();
-        if (v != tmp){
-            tmp = v;
-            if (v)
-                logService->log(LogLevel::INFO, "debuging", "HIGH");
-            else
-                logService->log(LogLevel::INFO, "debuging", "LOW");
-        }
-    });
+        auto vstpPrefix = storage->get("vstp.prefix", DEFAULT_PREFIX);
+        vssCli = new VSTP::VstpClient(scheduler, *conService, *logService, vstpPrefix);
 
-
-    //((WFService*)conService)->autoConnectOrCreateAp();
+        mainTelnetUI = new MainTelnetUI(telnetHelpTitleLine, *hal, *logService, *storage, *conService, *keyboard, vssCli, *telnetInterface);
+    /* #endregion } */
     
+    /* #region wifi startup */
+        ((WFService*)conService)->autoConnectOrCreateAp()->then([&](Error err) {
+            if (err != Errors::NoError) {
+                nLog.error(Errors::DerivateError(err, "Error while starting wifi management"));
+            } else {
+                nLog.info("Wifi management started successfully");
+            }
+        });
+    /* #endregion */
 
+    /* #region vss startup { */
+        vssCli->onConnectionStateChange.listen([&](VSTP::VstpClientState newState) {
+            if (newState == VSTP::VstpClientState::CONNECTED) {
+                auto wifiInfo = static_cast<WFService *>(conService)->getWifiCliAddrInfo();
+                vssCli->setVar("deviceInfo.network.ssid", wifiInfo.networkInfo.ssid);
+                vssCli->setVar("deviceInfo.network.ip", wifiInfo.ip);
+                vssCli->setVar("deviceInfo.network.macAddress", wifiInfo.macAddress);
+                vssCli->setVar("deviceInfo.firmwareVersion", VERSION);
+            }
+        });
+
+
+        auto vstpServer = storage->get("vstp.server", "");
+        auto vstpPortRaw = storage->get("vstp.port", "5032");
+        auto vstpPort = atoi(vstpPortRaw.c_str());
+
+        logService->log(LogLevel::INFO, "Main", "Starting VSS connection to server '" + vstpServer + "' on port " + String(vstpPort) + "'");
+        
+        auto err = vssCli->start(vstpServer, vstpPort);
+        if (err != Errors::NoError) {
+            nLog.error(Errors::DerivateError(err, "error starting VSTP client"));
+        } else {
+            nLog.info("VSTP client started successfully");
+        }
+    /* #endregion */
+
+    auto runs = storage->get("misc.runs", "0");
+    storage->set("misc.runs", String((runs.toInt() + 1)));
+    nLog.info("Run count: " + runs);
+
+    nLog.info("Main setup complete. Version: " + VERSION);
+
+    lockControl = new MainLockControl(
+        scheduler, 
+        *hal, 
+        *logService, 
+        MOTOR_LOCK_OUTPUT_PIN, 
+        MOTOR_UNLOCK_OUTPUT_PIN, 
+        ENDLINE_LOCK_INPUT_PIN, 
+        ENDLINE_UNLOCK_INPUT_PIN, 
+        MOTOR_ENABLE_OUTPUT_PIN
+    );
+
+    lockControl->OnStateChange.listen([&](std::tuple<MainLockControl::WorkingState, String> stateInfo){
+
+        auto state = MainLockControl::stateToString(std::get<0>(stateInfo));
+        auto description = std::get<1>(stateInfo);
+        vssCli->setVar("lockControl.state", state);
+        vssCli->setVar("lockControl.description", description);
+    });
+
+    keyboard->OnSpecificKeyPress(KEY_LOCK)->listen([&](IKeyboard::PressEvent event){
+        lockControl->Lock(timeout)->then([](MainLockControl::LockUnlockResult result){
+            if (result.err != Errors::NoError){
+                nLog.error(Errors::DerivateError(result.err, "error locking the door"));
+            } else {
+                nLog.debug("door locked successfully. Time taken: " + String(result.timeTaken) + "ms");
+            }
+        });
+    });
+
+    keyboard->OnSpecificKeyPress(KEY_UNLOCK)->listen([&](IKeyboard::PressEvent event){
+        lockControl->Unlock(timeout)->then([](MainLockControl::LockUnlockResult result){
+            if (result.err != Errors::NoError){
+                nLog.error(Errors::DerivateError(result.err, "error unlocking the door"));
+            } else {
+                nLog.debug("door unlocked successfully. Time taken: " + String(result.timeTaken) + "ms");
+            }
+        });
+    });
 }
 
-void loop() {
+void loop()
+{
     scheduler.run_one_round_of_tasks();
     ArduinoOTA.handle();
 }
-
-
-// KeyBoard.ListenPress(KEY_LOCK, [](int key, int totalTime){
-//     LockUnlock(true)->then([](Error err){ });
-// }
-
-// KeyBoard.ListenPress(KEY_UNLOCK, [](int key, int totalTime){
-//     LockUnlock(true)->then([](Error err){ });
-// }
-
-// //ListePressing is called periodicaly ~100ms while akey is pressed
-// KeyBoard.ListenPressing(KEY_ANY, [](int key, int currentPressingTime){
-//     LockUnlock(true)->then([](Error err){ });
-// }
-
-
-
-// #define VSS_LOCK_STATE_UNLOCKED "0"
-// #define VSS_LOCK_STATE_LOCKED "1"
-// #define VSS_LOCK_STATE_UNKNOWN "2"
-// #define VSS_LOCK_STATE_UNLOCKREQUESTED "10"
-// #define VSS_LOCK_STATE_LOCKREQUESTED "11"
-// #define VSS_LOCK_STATE_UNLOCKING "20"
-// #define VSS_LOCK_STATE_LOCKING "21"
-// #define VSS_LOCK_STATE_UNLOCKING_ERROR "30"
-// #define VSS_LOCK_STATE_LOCKING_ERROR "31"
-
-
-//Promise<Error>::smp_t LockUnLock(bool lock){
-//vss.set("lockStateError", "");
-//if locker.isWorking(){
-//    return Errors::AlreadyWorking;
-//    //abort lock/unlock
-//    locker.abort();
-//vss.set("lockState", VSS_LOCK_STATE_UNKNOWN);
-//}
-//    auto ret = Promise<Error>::getSmp();
-//
-//    auto finish = [=](Error err){
-//        if (err != Errors::NoError){
-//            auto err = Errors.New("Failed to "+(lock ? "lock" : "unlock")+": " + String(err));
-//            if lock {
-//                vss.set("lockState", VSS_LOCK_STATE_LOCKING_ERROR);
-//            } else {
-//                vss.set("lockState", VSS_LOCK_STATE_UNLOCKING_ERROR);
-//            }
-//
-//            vss.set("lockStateError", err);
-//            ret->post();
-//            return;
-//        }
-//
-//        if lock {
-//            vss.set("lockState", VSS_LOCK_STATE_LOCKED);
-//        } else {
-//            vss.set("lockState", VSS_LOCK_STATE_UNLOCKED);
-//        }
-//        ret->post(Errors::NoError);
-//        
-//    };
-//
-//    if lock (){
-//        vss.set("lockState", VSS_LOCK_STATE_LOCKING)
-//        locker.lock()->then(finish);
-//    } else {
-//        vss.set("lockState", VSS_LOCK_STATE_UNLOCKING)
-//        locker.unlock()->then(finish);
-//    }
-//}
-
-
--- src/main.lockcontrol.h
+++ src/main.lockcontrol.h
@@ -5,111 +5,226 @@
 
 using namespace ProcessChain;
 
-class LockControl {
-private:
+class MainLockControl {
+public:
     enum WorkingState{
-        IDLE,
+        UNKNOWN,
+        ABORT_REQUESTED,
+        ABORTED,
         LOCKING,
-        UNLOCKING
-    };    
+        UNLOCKING,
+        LOCKED,
+        UNLOCKED,
+        PANIC
+    };  
+private:
+    enum Direction{
+        DLock,
+        DUnlock
+    };
 
-    WorkingState currentState = IDLE;
+    WorkingState currentState = UNKNOWN;
 
     Scheduler &scheduler;
-    IIOHal &iohal;
+    IIOHal &hal;
+    NamedLog nLog;
 
-    IIOHAL_IO_ID_TYPE lockIoOut;
-    IIOHAL_IO_ID_TYPE unlockIoOut;
-    IIOHAL_IO_ID_TYPE lockLimitSwitchIoIn;
-    IIOHAL_IO_ID_TYPE unlockLimitSwitchIoIn;
+    IIOHAL_IO_ID_TYPE LOCK_OUTPUT_PIN;
+    IIOHAL_IO_ID_TYPE UNLOCK_OUTPUT_PIN;
+    IIOHAL_IO_ID_TYPE LOCK_LIMIT_INPUT_PIN;
+    IIOHAL_IO_ID_TYPE UNLOCK_LIMIT_INPUT_PIN;
+    IIOHAL_IO_ID_TYPE ENABLE_MOTOR_PIN;
+    
 public:
-    LockControl (
+
+    struct LockUnlockResult{
+        Error err;
+        uint timeTaken;
+    };
+  
+
+    MainLockControl (
         Scheduler &scheduler,
-        IIOHal &iohal, 
-        IIOHAL_IO_ID_TYPE lockIoOut,
-        IIOHAL_IO_ID_TYPE unlockIoOut,
-        IIOHAL_IO_ID_TYPE lockLimitSwitchIoIn,
-        IIOHAL_IO_ID_TYPE unlockLimitSwitchIoIn
+        IIOHal &iohal,
+        ILogger &logger, 
+        IIOHAL_IO_ID_TYPE PLOCK_OUTPUT_PIN,
+        IIOHAL_IO_ID_TYPE PUNLOCK_OUTPUT_PIN,
+        IIOHAL_IO_ID_TYPE PLOCK_LIMIT_INPUT_PIN,
+        IIOHAL_IO_ID_TYPE PUNLOCK_LIMIT_INPUT_PIN,
+        IIOHAL_IO_ID_TYPE PENABLE_MOTOR_PIN
     ):  scheduler(scheduler), 
-        iohal(iohal), 
-        lockIoOut(lockIoOut), 
-        unlockIoOut(unlockIoOut), 
-        lockLimitSwitchIoIn(lockLimitSwitchIoIn), 
-        unlockLimitSwitchIoIn(unlockLimitSwitchIoIn)
+        hal(iohal), 
+        LOCK_OUTPUT_PIN(PLOCK_OUTPUT_PIN), 
+        UNLOCK_OUTPUT_PIN(PUNLOCK_OUTPUT_PIN), 
+        LOCK_LIMIT_INPUT_PIN(PLOCK_LIMIT_INPUT_PIN), 
+        UNLOCK_LIMIT_INPUT_PIN(PUNLOCK_LIMIT_INPUT_PIN),
+        ENABLE_MOTOR_PIN(PENABLE_MOTOR_PIN)
     {
-        //constructor code here
+        nLog = logger.getNLog("Main::LockControl");
     }
 
 
-    Promise<Error>::type Lock(uint timeout){
-        return LockUnlock(true, timeout);
+    Promise<LockUnlockResult>::type Lock(uint timeout){
+        return lockUnlock(DLock, timeout);
     }
 
-    Promise<Error>::type Unlock(uint timeout){
-        return LockUnlock(false, timeout);
+    Promise<LockUnlockResult>::type Unlock(uint timeout){
+        return lockUnlock(DUnlock, timeout);
     }
 
-    Error AbortCurrentOperation(){
-        //abort current lock/unlock operation
-        auto err1 = iohal.Write(lockIoOut, false);
-        auto err2 = iohal.Write(unlockIoOut, false);
+    EventStream<std::tuple<WorkingState, String>> OnStateChange;
 
-        if (err1 != Errors::NoError && err2 != Errors::NoError)
-            return Errors::DerivateError(err1, "Failed to abort lock and unlock operations");
-        else if (err1 != Errors::NoError)
-            return Errors::DerivateError(err1, "Failed to abort lock operation");
-        else if (err2 != Errors::NoError)            
-            return Errors::DerivateError(err2, "Failed to abort unlock operation");
-
-        return Errors::NoError;
-    }
-
-    //returns true if there is a lock or unlock operation in progress
-    bool IsWorking(){
-        return currentState != WorkingState::IDLE;
-    }
-private:
-    Promise<Error>::type LockUnlock(bool lock, uint timeout){
-        auto ret = Promise<Error>::instance();
-        if (lock)
+    static String stateToString(WorkingState state){
+        switch (state)
         {
-            iohal.Write(lockIoOut, true);
-            currentState = WorkingState::LOCKING;
+        case UNKNOWN:
+            return "unknown";
+        case ABORTED:
+            return "aborted";
+        case LOCKING:
+            return "locking";
+        case UNLOCKING:
+            return "unlocking";
+        case LOCKED:
+            return "locked";
+        case UNLOCKED:
+            return "unlocked";
+        case PANIC:
+            return "panic";
+        default:
+            return "invalid_state: " + String((int)state);
+        }
+    }
+
+protected:
+    Promise<LockUnlockResult>::type lockUnlock(Direction direction, uint timeout){
+        auto ret = Promise<LockUnlockResult>::get_smp();
+
+        if ((direction == DLock)){
+            if (currentState == LOCKED){
+                ret->post({Errors::NoError, 0});
+                return ret;
+            } else if (currentState == LOCKING){
+                ret->post({Errors::NoError, 0});
+                return ret;
+            }
+
+        } else if (direction == DUnlock){
+            if (currentState == UNLOCKED){
+                ret->post({Errors::NoError, 0});
+                return ret;
+            } else if (currentState == UNLOCKING){
+                ret->post({Errors::NoError, 0});
+                return ret;
+            }
         } else {
-            iohal.Write(unlockIoOut, true);
-            currentState = WorkingState::UNLOCKING; 
+            ret->post({"Invalid Direction", 0});
+            return ret;
         }
 
-        scheduler.periodicTask(100, [=](std::shared_ptr<TimedTask> task){
-            bool lockLimitSwitchState = iohal.Read(lockLimitSwitchIoIn).Digital();
-            bool unlockLimitSwitchState = iohal.Read(unlockLimitSwitchIoIn).Digital();
-
-            if (lock && lockLimitSwitchState){
-                ret->post(Errors::NoError);
-                currentState = WorkingState::IDLE;
-                task->abort();
-
-            }
-            else if (!lock && unlockLimitSwitchState){
-                ret->post(Errors::NoError);
-                currentState = WorkingState::IDLE;
-
-                task->abort();
-            }
-
-            //check timeout
-            if (task->taskAge >= timeout){
-                auto err = this->AbortCurrentOperation();
-                currentState = WorkingState::IDLE;
-                if (err != Errors::NoError){
-                    ret->post(Errors::DerivateError(err, Errors::TimeoutError + " + Failed to abort lock/unlock operation after timeout"));
-                    return;
+        //abort a previus operation (if any).
+        if (currentState == LOCKING || currentState == UNLOCKING){
+            currentState = ABORT_REQUESTED;
+            scheduler.periodicTask(10 MiliSeconds, [=](shr_tmdtsk tasks){
+                if (currentState == ABORTED){
+                    lockUnlock(direction, timeout)->then([=](LockUnlockResult result){
+                        ret->post(result);
+                    });
+                    tasks->abort();
+                } else if (tasks->taskAge > 1000){
+                    ret->post({"can't abort a previous operation", tasks->taskAge});
+                    tasks->abort();
                 }
-                ret->post(Errors::TimeoutError);
+            }, "", DEFAULT_PRIORITIES::HIGH_);
+            return ret;
+        }
+
+        
+
+        nLog.debug("starting to " + String(direction == DLock ? "lock" : "unlock") + " with timeout " + String(timeout) + "ms");
+
+        auto PIN_TO_ENABLE = direction == DLock ? LOCK_OUTPUT_PIN : UNLOCK_OUTPUT_PIN;
+        auto PIN_TO_DISABLE = direction == DLock ? UNLOCK_OUTPUT_PIN : LOCK_OUTPUT_PIN;
+        auto ENDLINE_PIN = direction == DLock ? LOCK_LIMIT_INPUT_PIN : UNLOCK_LIMIT_INPUT_PIN;
+
+        
+
+        currentState = direction == DLock ? LOCKING : UNLOCKING;
+        OnStateChange.stream(
+            std::make_tuple(currentState, String(direction == DLock ? "locking" : "unlocking"))
+        );
+
+        hal.Write(PIN_TO_DISABLE, IIOHal::WriteValue::FromDigital(false)); //only for safety.
+        hal.Write(PIN_TO_ENABLE, IIOHal::WriteValue::FromDigital(true));
+
+        hal.Write(ENABLE_MOTOR_PIN, IIOHal::WriteValue::FromDigital(true));
+
+        scheduler.periodicTask(5 MiliSeconds, [=](shr_tmdtsk task){
+            //check if operation was aborted
+            if (currentState == ABORT_REQUESTED){
+                hal.Write(ENABLE_MOTOR_PIN, IIOHal::WriteValue::FromDigital(false));
+                hal.Write(LOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+                hal.Write(UNLOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+                currentState = ABORTED;
+                OnStateChange.stream(std::make_tuple(currentState, "action aborted. Time taken until abort: " + String(task->taskAge) + "ms / " + String(timeout) + "ms"));
+                ret->post({"action canceled", task->taskAge});
+                task->abort();  
+                return;
+            }
+
+            //read endline (if any error happens while reading endline, trigger 'panic' mode)
+            auto endline = hal.Read(ENDLINE_PIN);
+            if (endline.err != Errors::NoError){
+                String msg = "failed to "+String(direction == DLock ? "lock" : "unlock")+": error reading endline pin: " + endline.err;
+                lockPanic(msg);
+                ret->post({msg, task->taskAge});
+                return;
+            }
+
+            //if endline was reached, gracefully end the current process
+            if (endline.Digital()){
+                hal.Write(ENABLE_MOTOR_PIN, IIOHal::WriteValue::FromDigital(false));
+                hal.Write(PIN_TO_ENABLE, IIOHal::WriteValue::FromDigital(false));
+                currentState = direction == DLock ? LOCKED : UNLOCKED;
+                ret->post({Errors::NoError, task->taskAge});
+                OnStateChange.stream(std::make_tuple(currentState, String(direction == DLock ? "locked" : "unlocked") + ". Time taken: " + String(task->taskAge) + "ms / " + String(timeout) + "ms"));
+            } else if (task->taskAge > timeout){
+            //if timeout was reached, trigger 'panic' mode
+            
+                String msg = "failed to "+String(direction == DLock ? "lock" : "unlock")+": timeout (" + String(task->taskAge) + "ms / " + String(timeout) + "ms)";
+                lockPanic(msg);
+                ret->post({msg, task->taskAge});
+            }
+
+            //task only should run while locking or unlocking, if for some reason the state changes, abort the task. If abor was requested while task is running, let it be "gracefull aborted"
+            if (currentState != LOCKING && currentState != UNLOCKING && currentState != ABORT_REQUESTED){
                 task->abort();
             }
-        }, "LockUnlockTimeoutCheck", 'm');
+        }, "", DEFAULT_PRIORITIES::HIGH_);
 
         return ret;
+
     }
+
+    void abortCurrentAction(const String& reason = "unknown reason"){
+        hal.Write(ENABLE_MOTOR_PIN, IIOHal::WriteValue::FromDigital(false));
+        hal.Write(LOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+        hal.Write(UNLOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+        currentState = ABORT_REQUESTED;
+        OnStateChange.stream(std::make_tuple(currentState, reason));
+        nLog.warning("current action aborted: " + reason);
+    }
+
+    void lockPanic(const String& reason = "unknown reason"){
+        hal.Write(ENABLE_MOTOR_PIN, IIOHal::WriteValue::FromDigital(false));
+        hal.Write(LOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+        hal.Write(UNLOCK_OUTPUT_PIN, IIOHal::WriteValue::FromDigital(false));
+        currentState = PANIC;
+        nLog.critical("panic mode activated: " + reason);
+        OnStateChange.stream(std::make_tuple(currentState, reason));
+    }
+
+   
+private:
+    
 };
